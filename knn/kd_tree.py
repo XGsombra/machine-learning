b189f79817e62aee296c7a702d6403e39f0237d0
XGsombra <xuduo.gu@mail.utoronto.ca>
2022-06-05 07:47:42 -0400

    Implemented search with little bug

--- knn/kd_tree.py
+++ knn/kd_tree.py
@@ -1,12 +1,25 @@
 import numpy as np
 
 
+def calculate_dist(x1, x2):
+    diff = x1 - x2
+    return np.sqrt(np.dot(diff.T, diff))
+
+
 class KDTreeNode:
+    """
+    The class for kd-tree
+    """
+
     def __init__(self, x):
         self.x = x
-        self.axis_idx = -1
+        self.axis_feature_idx = -1
         self.left = None
         self.right = None
+        self.parent = None
+
+    def __repr__(self):
+        return KDTreeNode.print_kd_tree([self])
 
     @staticmethod
     def print_kd_tree(stack):
@@ -23,12 +36,9 @@ class KDTreeNode:
         output += "\n"
         return output + KDTreeNode.print_kd_tree(new_stack)
 
-    def __repr__(self):
-        return KDTreeNode.print_kd_tree([self])
-
     @staticmethod
-    def construct(data, feature_idx):
-        """ 
+    def construct(data, feature_idx, parent=None):
+        """
         DATA: A non-empty numpy array of data.
         FEATURE_IDX: The index at which the data are splited.
 
@@ -41,7 +51,8 @@ class KDTreeNode:
         axis_idx = np.argsort(feature_column)[n//2]
         axis = data[axis_idx]
         root = KDTreeNode(axis)
-        root.axis_idx = axis_idx
+        root.axis_feature_idx = feature_idx
+        root.parent = parent
         # Base case where data has only one entry.
         if n == 1:
             return root
@@ -58,15 +69,88 @@ class KDTreeNode:
         next_feature_idx = 0 if feature_idx == k - 1 else feature_idx + 1
         if len(left_nodes) > 0:
             root.left = KDTreeNode.construct(
-                np.array(left_nodes), next_feature_idx)
+                np.array(left_nodes), next_feature_idx, root)
         if len(right_nodes) > 0:
             root.right = KDTreeNode.construct(
-                np.array(right_nodes), next_feature_idx)
+                np.array(right_nodes), next_feature_idx, root)
         return root
+
+    def search(self, x):
+        """
+        Search to the nearest leaf node
+        """
+        curr = self
+        # Search to the leaf node
+        while curr.left is not None and curr.right is not None:
+            if curr.left is None:
+                curr = curr.right
+            if curr.right is None:
+                curr = curr.left
+            curr = curr.left if x[curr.axis_feature_idx] < curr.x[curr.axis_feature_idx] else curr.right
+        return curr
+
+    @staticmethod
+    def insert_sort(arr, curr_x, x):
+        i = 0
+        curr_x_x_dist = calculate_dist(x, curr_x)
+        while i < len(arr) and calculate_dist(arr[i], x) < curr_x_x_dist:
+            i += 1
+        return arr[:i] + [curr_x] + arr[i:]
+
+    def find_knn(self, x, k):
+        """
+        Given a data entry x who has the same size as the kd-tree nodes,
+        return a list of the top k nearest neighbors of x
+        """
+
+        L = []
+        searched_nodes = set([])
+        curr = self
+        while True:
+            # Run a binary search
+            curr = curr.search(x)
+            # Add the node
+            searched_nodes.add(curr)
+            if len(L) < k + 1:
+                L = KDTreeNode.insert_sort(L, curr.x, x)
+                if len(L) == k + 1:
+                    L.pop()
+            # Go up
+            while True:
+                if curr.parent is None:
+                    return L
+                else:
+                    prev = curr
+                    curr = curr.parent
+                    if curr not in searched_nodes:
+                        searched_nodes.add(curr)
+                        if len(L) < k + 1:
+                            L = KDTreeNode.insert_sort(L, curr.x, x)
+                            if len(L) == k + 1:
+                                L.pop()
+                        axis_feature_idx = curr.axis_feature_idx
+                        dist_to_axis = abs(
+                            curr.x[axis_feature_idx] - x[axis_feature_idx])
+                        if dist_to_axis >= calculate_dist(L[-1], x) and len(L) == k:
+                            continue
+                        else:
+                            curr = curr.left if prev == curr.right else curr.right
+                            break
 
 
 if __name__ == "__main__":
-    # data = np.random.rand(7, 2)
-    # tree = KDTreeNode.construct(data, 0)
-    # print(tree)
-    pass
+    data = np.random.rand(100, 5)
+    tree = KDTreeNode.construct(data, 0)
+    print(tree)
+
+    k = 6
+    target = np.array([0.5, 0.5, 0.5, 0.5, 0.5])
+    expected = []
+    for x in data:
+        expected.append(calculate_dist(x, target))
+    expected.sort()
+    print(expected[:k])
+    actual = []
+    for x in tree.find_knn(target, k):
+        actual.append(calculate_dist(x, target))
+    print(actual)
