467d64a0184df055d619b0b38e3110d48808b3d6
Xuduo Gu <xuduo.gu@mail.utoronto.ca>
2022-07-04 11:03:37 -0400

    Implemented boosting tree

--- adaboost/AdaBoost.py
+++ adaboost/AdaBoost.py
@@ -2,10 +2,36 @@ import numpy as np
 from Stump import Stump 
 
 X = np.array([[0], [1], [2], [3], [4], [5], [6], [7], [8], [9]])
-Y = np.array([1, 1, 1, -1, -1, -1, 1, 1, 1, -1])
-W = np.ones(10) / 10
+Y = np.array([[1], [1], [1], [-1], [-1], [-1], [1], [1], [1], [-1]])
+
+class BoostingTree:
+    def __init__(self):
+        self.W = None
+        self.e = None
+        self.classifier = None
+        
+    def train(self, X, Y, iteration=100):
+        N = X.shape[0]
+        self.W = np.ones((X.shape[0], 1)) / N
+        F = []
+        A = []
+        for i in range(iteration):
+            stump = Stump()
+            e = stump.train(X, Y, self.W)
+            if e >= .5:
+                break
+            a = np.log((1-e)/e)/2
+            self.W = np.array([self.W[i] * np.exp(-a*stump.f(X[i])*Y[i]) for i in range(N)]) # can be optimized using vectorization
+            self.W /= np.sum(self.W)
+            print(self.W)
+            F.append(stump.f)
+            A.append(a)
+            #print(stump)
+        print(A)
+        self.classifier = lambda x: 1 if sum([A[i] * F[i](x) for i in range(len(A))]) > 0 else -1
+        return self
 
 if __name__=="__main__":
-    stump = Stump()
-    stump.train(X, Y, W)
-    print(stump)
+    b = BoostingTree()
+    b.train(X, Y, 3)
+    print(b.classifier(X[3]))
--- adaboost/Stump.py
+++ adaboost/Stump.py
@@ -1,8 +1,7 @@
 import numpy as np
 
 class Stump:
-    def __init__(self, step=1):
-        self.step = step
+    def __init__(self):
         self.f = None
         self.threshold = None
         self.feature_idx = None
@@ -23,11 +22,14 @@ class Stump:
         e = 0
         for i in range(X.shape[0]):
             e += W[i] * 1 \
-            if ((X[i, feature_idx] - t) * Y[i] < 0 if high_one else (X[i, feature_idx] - t) * Y[i] > 0) \
-            else 0
-        return e
+              if ((X[i, feature_idx] - t) * Y[i, 0] < 0 if high_one else (X[i, feature_idx] - t) * Y[i, 0] > 0) \
+              else 0
+        return e[0]
      
-    def train(self, X, Y, W, step=1):
+    def train(self, X, Y, W, step=1.0):
+        """
+        Train the stump and return the error (as in a boosting tree)
+        """
         e = 1.0 # error rate
         for f_idx in range(X.shape[1]):
             low = min(X[:,f_idx])
@@ -35,13 +37,15 @@ class Stump:
             t = low - step / 2 # to cover the seperation that groups all the data points
             while t <= high + step / 2:
                 for high_one in [True, False]:
-                    new_e = Stump.calculate_error_rate(X, Y, W, t, f_idx, high_one)    
+                    new_e = Stump.calculate_error_rate(X, Y, W, t, f_idx, high_one) 
                     if new_e < e:
                         e = new_e
                         self.threshold = t
                         self.feature_idx = f_idx
                         self.high_one = high_one
                 t += step
-        self.e = e
-        self.f = lambda x: 1 if x > self.threshold else -1
-        return self
+        self.f = lambda x: 1 \
+            if (self.high_one and x[self.feature_idx] > self.threshold) or \
+                (not self.high_one and x[self.feature_idx] < self.threshold)  \
+                    else -1
+        return e
