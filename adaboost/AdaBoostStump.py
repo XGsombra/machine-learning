d907c9657c64c5db65f338fb411c5ea12fb4bc0d
Xuduo Gu <xuduo.gu@mail.utoronto.ca>
2022-07-05 07:57:10 -0400

    Reformated file

--- adaboost/AdaBoostStump.py
+++ adaboost/AdaBoostStump.py
@@ -1,10 +1,7 @@
 import numpy as np
 from Stump import Stump 
 
-X = np.array([[0], [1], [2], [3], [4], [5], [6], [7], [8], [9]])
-Y = np.array([[1], [1], [1], [-1], [-1], [-1], [1], [1], [1], [-1]])
-
-class BoostingTree:
+class AdaBoostStump:
     def __init__(self):
         self.W = None
         self.e = None
@@ -23,15 +20,14 @@ class BoostingTree:
             a = np.log((1-e)/e)/2
             self.W = np.array([self.W[i] * np.exp(-a*stump.f(X[i])*Y[i]) for i in range(N)]) # can be optimized using vectorization
             self.W /= np.sum(self.W)
-            print(self.W)
             F.append(stump.f)
             A.append(a)
-            #print(stump)
-        print(A)
         self.classifier = lambda x: 1 if sum([A[i] * F[i](x) for i in range(len(A))]) > 0 else -1
         return self
 
 if __name__=="__main__":
-    b = BoostingTree()
+    X = np.array([[0], [1], [2], [3], [4], [5], [6], [7], [8], [9]])
+    Y = np.array([[1], [1], [1], [-1], [-1], [-1], [1], [1], [1], [-1]])
+    b = AdaBoostStump()
     b.train(X, Y, 3)
     print(b.classifier(X[3]))
